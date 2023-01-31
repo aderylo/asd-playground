7d6bf99ba9c0e3b416fbfcea7b1620eed32d2a7a
ad432952 <a.m.derylo@gmail.com>
2023-01-31 14:44:14 +0100

    Add setup for POD

---
+++ egzamin-2008/POD/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+#define ll long long
+
+using namespace std;
+
+int main(int, char **) {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(NULL);
+
+  return 0;
+}
