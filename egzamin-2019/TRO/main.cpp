26a0a120ee40e2371cf592abcfed8897ed8d008b
Adam Derylo <a.m.derylo@gmail.com>
2023-02-01 14:48:59 +0100

    init trojki

---
+++ egzamin-2019/TRO/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+
+using namespace std;
+
+
+int main(int, char**) {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(NULL);
+
+  std::cout << "Hello, world!\n";
+
+
+  return 0;
+}
